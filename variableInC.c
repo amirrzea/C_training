9af30849fac4b8fb26eb83849f7eff260bf4b23d
amirrzea <amirrzea@gmail.com>
2023-06-23 09:34:55 -0400

    Constant commit

--- variableInC.c
+++ variableInC.c
@@ -31,7 +31,36 @@ int main()
     // Printing variables
     printf(" %d \n %d \n %f \n %f \n %c \n %c", a, b, c, d=4.5, e, f='B');
     printf("\nMy favorite number is: %d", a);
-    printf("\nMy number is %d and my letter is %c", b, f);
+    printf("\nMy number is %d and my letter is %c\n", b, f);
+
+
+
+    float myFloatNum = 3.5;
+
+    printf("%f\n", myFloatNum); // Default will show 6 digits after the decimal point
+    printf("%.1f\n", myFloatNum); // Only show 1 digit
+    printf("%.2f\n", myFloatNum); // Only show 2 digits
+    printf("%.4f\n\n", myFloatNum);   // Only show 4 
+    
+
+
+    // Automatic conversion: int to float
+    float myFloat = 9;
+    printf("%f\n", myFloat); // 9.000000
+
+    // Manual conversion: int to float
+    int num1 = 5;
+    int num2 = 2;
+    float sum1 = (float) num1 / num2;
+    printf("%.1f", sum1); // 2.5
+
+
+    // If you don't want others (or yourself) to change existing variable values, you can use the const keyword
+    // This will declare the variable as "constant", which means unchangeable and read-only
+    const int minutesPerHour = 60;
+    const float PI = 3.14;
+
+
 
     return 0;
 }
