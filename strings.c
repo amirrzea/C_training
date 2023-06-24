96eb05975a7e5a055339783465aa36ea973d97ad
amirrzea <amirrzea@gmail.com>
2023-06-24 16:28:36 -0400

    Special chars commit

--- strings.c
+++ strings.c
@@ -15,6 +15,13 @@ int main(){
         printf("%c ", carName[i]);
     }
     
+    // Special characters -> ' , " , 
+    char txt[] = "We are the so-called \"Vikings\" from the north.";
+    char txt[] = "It\'s alright.";
+    char txt[] = "The character \\ is called backslash.";
 
+    printf("\n"); // New Line
+    printf("\t"); //Tab
+    printf("\0"); // Null
     return 0;
 }
