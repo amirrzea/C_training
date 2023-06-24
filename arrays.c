1873238c158d08903b9e1937d554b846c831fc55
amirrzea <amirrzea@gmail.com>
2023-06-23 23:01:54 -0400

    arrays commit

--- arrays.c
+++ arrays.c
@@ -6,5 +6,21 @@ int main(){
 
     int myNumbers[]={25,50,75,100};
     printf("%d", myNumbers[3]);
+
+    //changing the array
+    myNumbers[3]=55;
+    printf("\n%d\n\n",myNumbers[3]);
+
+
+    //Loop through th array
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\n%d",myNumbers[i]);
+    }
+    
+
+    // Another way of defining array
+    int myNumber2[5];
+    
     return 0;
 }
