0de086d3a81acd1d4892210c9ea6465cd32d7c45
amirrzea <amirrzea@gmail.com>
2023-06-23 23:08:23 -0400

    Array 2 commit

--- arrays.c
+++ arrays.c
@@ -15,12 +15,28 @@ int main(){
     //Loop through th array
     for (int i = 0; i < 4; i++)
     {
-        printf("\n%d",myNumbers[i]);
+        printf("\n%d\n",myNumbers[i]);
     }
     
 
     // Another way of defining array
     int myNumber2[5];
+
+
+    // Two-dimentional Arrays
+    int matrix[2][3] = {{1,3,4}, {6,8,10}};
+
+    // Loop through the 2D array
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\n", matrix[i][j]);
+        }
+        
+    }
     
+
+
     return 0;
 }
